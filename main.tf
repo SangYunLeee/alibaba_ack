7072e2311da4cd1b4f74d6448f53c9e2dc77a0ba
Lee-Sang-Yun <sylee@ontacthealth.com>
2025-04-21 13:43:37 +0900

    feat: 보안 그룹 및 규칙 추가
    - 보안 그룹 리소스 추가
    - 특정 IP(211.218.18.43)에서의 SSH 접속만 허용하는 보안 그룹 규칙 추가
    - 변수 설명 개선 및 기본값 제거

--- main.tf
+++ main.tf
@@ -10,4 +10,24 @@ resource "alicloud_vswitch" "main" {
   cidr_block        = var.vswitch_cidrs[0]
   zone_id           = var.availability_zones[0]
   vswitch_name      = "${var.vpc_name}-vswitch"
+}
+
+# 보안 그룹 생성
+resource "alicloud_security_group" "main" {
+  name        = "${var.vpc_name}-sg"
+  vpc_id      = alicloud_vpc.main.id
+  description = "Security group for ${var.vpc_name}"
+}
+
+# 보안 그룹 규칙 추가 (예시: SSH 접속 허용)
+resource "alicloud_security_group_rule" "allow_ssh" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "22/22"
+  priority          = 1
+  security_group_id = alicloud_security_group.main.id
+  cidr_ip           = "211.218.18.43/32"
+  description       = "Allow SSH access from specific IP range"
 } 
--- variables.tf
+++ variables.tf
@@ -1,23 +1,19 @@
 variable "vpc_name" {
-  description = "VPC의 이름"
+  description = "VPC 이름"
   type        = string
-  default     = "my-vpc"
 }
 
 variable "vpc_cidr" {
-  description = "VPC의 CIDR 블록"
+  description = "VPC CIDR 블록"
   type        = string
-  default     = "172.31.0.0/16"  # 65,536개 IP
 }
 
 variable "vswitch_cidrs" {
-  description = "VSwitch들의 CIDR 블록"
+  description = "VSwitch CIDR 블록 목록"
   type        = list(string)
-  default     = ["172.31.0.0/20"]  # 4,096개 IP
 }
 
 variable "availability_zones" {
   description = "가용 영역 목록"
   type        = list(string)
-  default     = ["cn-hangzhou-g"]  # Hangzhou Zone G
 }
